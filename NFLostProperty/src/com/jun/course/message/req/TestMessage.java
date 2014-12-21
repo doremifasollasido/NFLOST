32c58424684a5fd9ac63d2c178bb18db95514ae3
doremifasollasido <624034495@qq.com>
2014-12-21 22:51:33 +0800

    FDG

---
+++ NFLostProperty/src/com/jun/course/message/req/TestMessage.java
@@ -0,0 +1,18 @@
+package com.jun.course.message.req;
+/**
+ * 文本消息 
+ * @author Administrator
+ *
+ */
+public class TestMessage extends BaseMessage {
+	// 消息内容
+	private String Content;
+
+	public String getContent() {
+		return Content;
+	}
+
+	public void setContent(String content) {
+		Content = content;
+	}
+}
---
+++ NFLostProperty/src/com/jun/course/message/resp/MusicMessage.java
@@ -0,0 +1,19 @@
+package com.jun.course.message.resp;
+
+/** 
+ * ÒôÀÖÏûÏ¢ 
+ *  
+ */  
+public class MusicMessage extends BaseMessage {  
+    // ÒôÀÖ   
+    private Music Music;  
+  
+    public Music getMusic() {  
+        return Music;  
+    }  
+  
+    public void setMusic(Music music) {  
+        Music = music;  
+    }  
+}  
+
